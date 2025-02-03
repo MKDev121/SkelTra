1277481a5454f6c14a1a1e20972cd1bfab4844fd
MKDev121 <meatk121@gmail.com>
2025-02-03 11:26:40 +0530

    Added character.py

---
+++ character.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+class Character:
+    def __init__(self):
+        self.Rig=Rig
+        
+
+class Rig:
+
+    def __init__(self):
+        self.bones={}
+        self.position=pg.Vector3(0,0,0)
+        self.rotation=[0,0,0]
+
+class Sprite:
+
+    def __init__(self):
+        pass
+
